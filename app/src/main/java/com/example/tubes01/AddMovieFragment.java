880c160f5f93a68b80f8854563991ef54190cb2d
sharonkeziaf <6181901006@student.unpar.ac.id>
2021-10-19 14:32:33 +0700

    Urusan toolbar & benerin context tiap fragment

--- app/src/main/java/com/example/tubes01/AddMovieFragment.java
+++ app/src/main/java/com/example/tubes01/AddMovieFragment.java
@@ -23,6 +23,7 @@ public class AddMovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_movie, container, false);
+
         return view;
     }
 }
--- app/src/main/java/com/example/tubes01/MainActivity.java
+++ app/src/main/java/com/example/tubes01/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.tubes01;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tubes01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+//        setContentView(R.layout.activity_main);
+
+        //toolbar
+        this.toolbar = findViewById(R.id.toolbar);
+        this.toolbar.setTitle("Ma Montre"); //Judul aplikasi di toolbar
+        this.setSupportActionBar(this.toolbar);
+
+
+        //tombol garis tiga
+        DrawerLayout drawer  = this.binding.drawerLayout;
+        ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        drawer.addDrawerListener(abdt);
+        abdt.syncState();
+
+
+
 
         //page 1
 //        HomeFragment home = HomeFragment.newInstance();
@@ -21,11 +48,11 @@ public class MainActivity extends AppCompatActivity {
 //                .commit();
 
         //page 3
-//        AddMovieFragment addMovie = AddMovieFragment.newInstance();
-//        FragmentManager fragmentManager = this.getSupportFragmentManager();
-//        FragmentTransaction ft = fragmentManager.beginTransaction();
-//        ft.add(R.id.fragment_container, addMovie)
-//                .commit();
+        AddMovieFragment addMovie = AddMovieFragment.newInstance();
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.fragment_container, addMovie)
+                .commit();
 
         //page 4
 //        AddSeriesFragment addSeries = AddSeriesFragment.newInstance();
@@ -56,10 +83,10 @@ public class MainActivity extends AppCompatActivity {
 //                .commit();
 
         //page 7
-        ReviewPageFragment reviewPage = ReviewPageFragment.newInstance();
-        FragmentManager fragmentManager = this.getSupportFragmentManager();
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.add(R.id.fragment_container, reviewPage)
-                .commit();
+//        ReviewPageFragment reviewPage = ReviewPageFragment.newInstance();
+//        FragmentManager fragmentManager = this.getSupportFragmentManager();
+//        FragmentTransaction ft = fragmentManager.beginTransaction();
+//        ft.add(R.id.fragment_container, reviewPage)
+//                .commit();
     }
 }
