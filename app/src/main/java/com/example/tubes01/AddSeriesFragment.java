3911d2b7103f37119ff5b8f29281c0f9854b734a
sharonkeziaf <6181901006@student.unpar.ac.id>
2021-10-27 21:24:54 +0700

    edit series page

--- app/src/main/java/com/example/tubes01/AddSeriesFragment.java
+++ app/src/main/java/com/example/tubes01/AddSeriesFragment.java
@@ -1,17 +1,23 @@
 package com.example.tubes01;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.tubes01.databinding.FragmentAddSeriesBinding;
 import com.example.tubes01.databinding.FragmentFilmListBinding;
 
+import java.io.IOException;
 import java.util.List;
 
 public class AddSeriesFragment extends Fragment implements View.OnClickListener, IMainActivity{
@@ -19,6 +25,7 @@ public class AddSeriesFragment extends Fragment implements View.OnClickListener,
     private FragmentAddSeriesBinding binding;
     private MainPresenter presenter;
     private SeriesListAdapter adapter;
+    private Bitmap bitmap;
 
     public AddSeriesFragment(MainActivity activity){
         this.activity = activity;
@@ -46,7 +53,7 @@ public class AddSeriesFragment extends Fragment implements View.OnClickListener,
     @Override
     public void onClick(View view) {
         if(view == this.binding.asBtnUpload){
-
+            startActivityForResult(Intent.createChooser(this.presenter.getImageFromGallery(),"Select Picture"),1);
         }
         else{
             String title = this.binding.asBtnAdd.getText().toString();
@@ -61,6 +68,9 @@ public class AddSeriesFragment extends Fragment implements View.OnClickListener,
             }
             else if(synopsis.length()==0){
                 Toast.makeText(this.getContext(),"Please input film synopsis !",Toast.LENGTH_LONG).show();
+            }
+            else if(this.bitmap==null){
+                Toast.makeText(this.getContext(),"Please input film poster !",Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -95,5 +105,19 @@ public class AddSeriesFragment extends Fragment implements View.OnClickListener,
     @Override
     public void makeToastMessage(String message) {
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            Uri uri = data.getData();
+            try {
+                this.bitmap = MediaStore.Images.Media.getBitmap(this.activity.getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            this.binding.asPoster.setImageBitmap(this.bitmap);
+        }
     }
 }
